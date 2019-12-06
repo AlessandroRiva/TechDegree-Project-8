a1d23392fbde6eb0d5676243253b89180d4997e8
Alessandro Riva <alessandrovittorioriva@gmail.com>
2019-12-06 00:15:47 -0800

    Update

---
+++ app.js
@@ -0,0 +1,38 @@
+function createNode(element)
+{
+    return document.createElement(element);
+}
+
+function append(parent, el)
+{
+    return parent.appendChild(el);
+}
+
+const ul = document.getElementById('employees');
+
+const url = 'https://randomuser.me/api/?results=12';
+
+fetch(url)
+.then((resp) => resp.json())
+.then(function(data) {
+    let employees = data.results;
+
+    return authors.map(function(employee) {
+
+        let li = createNode('li'),
+        img = createNode('img')
+        span = createNode('span')
+
+        img.src=employee.picture.medium;
+
+        span.innerHTML = `${employee.name.first} ${employee.name.last}`;
+        append(li, img);
+        append(li, span);
+        append(ul,li);
+    })
+})
+
+.catch(function(error){
+
+    console.log(JSON.stringify(error));
+})
