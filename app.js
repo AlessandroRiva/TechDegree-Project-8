ee23a2f1532115af9d07e13f8312a588fa18bb46
Alessandro Riva <alessandrovittorioriva@gmail.com>
2019-12-07 14:33:33 -0800

    Update

--- app.js
+++ app.js
@@ -51,14 +51,43 @@ fetch(url)
     console.log(JSON.stringify(error));
 })
 
-const lightBox = document.querySelector('.lightbox');
 const li = document.querySelector('.employee-card');
 
-lightBox.classList.add('hidden');
+function displayModal(index) {
+    // use object destructuring make our template literal cleaner
+    let { name, dob, phone, email, location: { city, street, state, postcode
+    }, picture } = employees[index];
+    let date = new Date(dob.date);
+    const modalHTML = `
+    <img class="avatar" src="${picture.large}" />
+    <div class="text-container">
+    <h2 class="name">${name.first} ${name.last}</h2>
+    <p class="email">${email}</p>
+    <p class="address">${city}</p>
+    <hr />
+    <p>${phone}</p>
+    <p class="address">${street}, ${state} ${postcode}</p>
+    <p>Birthday:
+    ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+    </div>
+    `;
+    overlay.classList.remove("hidden");
+    modalContainer.innerHTML = modalHTML;
+}
 
-    li.addEventListener('click', () => {
-        lightBox.classList.add('active');
-    })
-    
+const containerEmployees = document.querySelector('.container-employees');
+let employees = [];
+const overlay = document.querySelector(".overlay");
+const modalContainer = document.querySelector(".modal-content");
+const modalClose = document.querySelector(".modal-close");
+
+containerEmployees.addEventListener('click', e => {
+    const card = e.target.closest(".card");
+    const index = card.getAttribute('data-index');
+    displayModal(index);
+    });
+    modalClose.addEventListener('click', () => {
+        overlay.classList.add("hidden");
+        });
 
 
