fa6e6eb44119758068d210d922e0d654f18f6a50
Alessandro Riva <alessandrovittorioriva@gmail.com>
2019-12-07 22:35:41 -0800

    Update

--- app.js
+++ app.js
@@ -1,60 +1,42 @@
-const containerEmployees = document.querySelector('.container-employees');
 let employees = [];
+const urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture,
+email, location, phone, dob &noinfo &nat=US`
+const gridContainer = document.querySelector(".grid-container");
 const overlay = document.querySelector(".overlay");
 const modalContainer = document.querySelector(".modal-content");
 const modalClose = document.querySelector(".modal-close");
-const ul = document.getElementById('employees');
-const url = 'https://randomuser.me/api/?results=12';
-const li = document.querySelector('.employee-card');
 
-function createNode(element)
-{
-    return document.createElement(element);
+fetch(urlAPI)
+.then(res => res.json())
+.then(res => res.results)
+.then(displayEmployees)
+.catch(err => console.log(err))
+
+function displayEmployees(employeeData) {
+    employees = employeeData;
+    // store the employee HTML as we create it
+    let employeeHTML = '';
+    // loop through each employee and create HTML markup
+    employees.forEach((employee, index) => {
+    let name = employee.name;
+    let email = employee.email;
+    let city = employee.location.city;
+    let picture = employee.picture;
+    // template literals make this so much cleaner!
+
+    employeeHTML += `
+    <div class="card" data-index="${index}">
+    <img class="avatar" src="${picture.large}" />
+    <div class="text-container">
+    <h2 class="name">${name.first} ${name.last}</h2>
+    <p class="email">${email}</p>
+    <p class="address">${city}</p>
+    </div>
+    </div>
+    `
+});
+gridContainer.innerHTML = employeeHTML;
 }
-
-function append(parent, el)
-{
-    return parent.appendChild(el);
-}
-
-fetch(url)
-.then((resp) => resp.json())
-.then((data) => {
-    let employees = data.results;
-
-    return employees.map((employee) => {
-
-        let li = createNode('li'),
-        img = createNode('img'),
-        name = createNode('div'),
-        email = createNode('div'),
-        city = createNode('div'),
-        containerInfo = createNode('div');
-
-        li.classList.add('employee-card');
-        containerInfo.classList.add('container-info');
-        name.classList.add('name');
-        email.classList.add('email');
-        city.classList.add('city');
-
-        img.src = employee.picture.large;
-
-        name.innerHTML = `${employee.name.first} ${employee.name.last}`;
-        email.innerHTML = `${employee.email}`;
-        city.innerHTML = `${employee.location.city}`;
-
-        append(li, img);
-        append(containerInfo, name);
-        append(containerInfo, email);
-        append(containerInfo, city);
-        append(li, containerInfo)
-        append(ul, li);
-    });
-})
-
-.catch((error) => {
-    console.log(JSON.stringify(error));
-})
 
 function displayModal(index) {
     // use object destructuring make our template literal cleaner
@@ -78,14 +60,16 @@ function displayModal(index) {
     modalContainer.innerHTML = modalHTML;
 }
 
-
-containerEmployees.addEventListener('click', e => {
-    const card = e.target.closest(".employee-card");
+gridContainer.addEventListener('click', e => {
+    // make sure the click is not on the gridContainer itself
+    if (e.target !== gridContainer) {
+    // select the card element based on its proximity to actual element
+    const card = e.target.closest(".card");
     const index = card.getAttribute('data-index');
     displayModal(index);
-    });
-    modalClose.addEventListener('click', () => {
-        overlay.classList.add("hidden");
-        });
+    }
+});
 
-
+modalClose.addEventListener('click', () => {
+    overlay.classList.add("hidden");
+});
